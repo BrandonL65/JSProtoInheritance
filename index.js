b3988bf1a73217a844edfa324780335194b53ce5
Brandon Lau <brandon.lau86@gmail.com>
2019-12-25 17:06:56 -0500

    add index.html

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 
-function Animal(name, species) 
-{
-  this.name = name;
-  this.species = species;
-  this.sayName = function() {
-    console.log(`Hello, my name is ${this.name}`)
+
+
+
+let intermediateObject = {
+  sayHi: function() {
+    console.log("HI")
   }
 }
 
-let tigre = new Animal("tigre", "tiger");
-console.log(tigre);
-tigre.sayName();
+let Brandon = {
+  name: "Brandon",
+  hobby: "Coding"
+}
+Brandon.__proto__ = intermediateObject;
+
+console.log(Brandon);
+// function Animal(name, species) 
+// {
+//   this.name = name;
+//   this.species = species;
+//   this.sayName = function() {
+//     console.log(`Hello, my name is ${this.name}`)
+//   }
+// }
+
+// let tigre = new Animal("tigre", "tiger");
+// console.log(tigre);
+// tigre.sayName();
