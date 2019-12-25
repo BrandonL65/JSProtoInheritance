6778a3d30bea9d1a8be0e4ebc2c14ad45e943a41
Brandon Lau <brandon.lau86@gmail.com>
2019-12-25 17:08:13 -0500

    clean up code

--- index.js
+++ index.js
@@ -17,15 +17,3 @@ Brandon.__proto__ = intermediateObject;
 console.log(Brandon);
 Brandon.sayHi();
 Brandon.toString();
-// function Animal(name, species) 
-// {
-//   this.name = name;
-//   this.species = species;
-//   this.sayName = function() {
-//     console.log(`Hello, my name is ${this.name}`)
-//   }
-// }
-
-// let tigre = new Animal("tigre", "tiger");
-// console.log(tigre);
-// tigre.sayName();
